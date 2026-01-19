c7dfad33900365863c9cd073d0e09bd7bd28f09f
Shawn Kearney <shawnpk29@me.com>
2026-01-19 11:34:59 -0700

    Generate cart scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_19_172404) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_19_183206) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -37,6 +37,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_19_172404) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
