2e44c19ebcf6eab12a48e47e58182ca38cad806d
Shawn Kearney <shawnpk29@me.com>
2026-01-19 11:39:34 -0700

    Generate line items scaffold

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
   has_one_attached :image
 
   after_commit -> { broadcast_refresh_later_to "products" }
+  before_destroy :ensure_not_referenced_by_any_line_items
 
   validate :acceptable_image
 
@@ -15,6 +18,16 @@ class Product < ApplicationRecord
     acceptable_types = [ "image/gif", "image/jpeg", "image/png" ]
     unless acceptable_types.include?(image.content_type)
       errors.add(:image, "must be a GIF, JPEG or PNG image")
+    end
+  end
+
+  private
+
+  def ensure_not_referenced_by_any_line_items
+    unless line_items.empty?
+      errors.add(:base, "Line Items present")
+
+      throw :abort
     end
   end
 end
